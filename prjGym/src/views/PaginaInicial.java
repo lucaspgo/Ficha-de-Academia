4467c0bed0f0d45373064b4f07bc4675e96a692d
LuPGO1 <62369806+LuPGO1@users.noreply.github.com>
2020-05-05 15:47:03 -0300

    Textos Pagina Inicial
    - Textos da página inicial foram feitos

--- prjGym/src/views/PaginaInicial.java
+++ prjGym/src/views/PaginaInicial.java
@@ -1,22 +1,45 @@
 package views;
 
 import models.Usuario;
+import utils.Console;
 
 public class PaginaInicial {
 	public static void renderizar(Usuario sessionUsuario) {
 		if(sessionUsuario.getTipo().equals("1"))
-			menuProfessor();
+			menuProfessor(sessionUsuario);
 		else
-			menuAluno();
-		
-		
+			menuAluno(sessionUsuario);
 	}
 	
-	public static void menuAluno() {
+	public static void menuAluno(Usuario sessionUsuario) {
+		System.out.println("--- SEJA BEM-VINDO ALUNO(A)" +sessionUsuario.getNome().toUpperCase()+" ---");
+		System.out.println("1 - VER FICHA");
+		System.out.println("2 - SAIR");
+		int opcao = Console.readInt("Selecione a opção desejada:");
 		
+		switch(opcao) {
+			case 1:
+				break;
+				
+			case 2:
+				break;				
+		}		
 	}
 	
-	public static void menuProfessor() {
+	public static void menuProfessor(Usuario sessionUsuario) {
+		System.out.println("--- SEJA BEM-VINDO PROFESSOR(A) "+sessionUsuario.getNome().toUpperCase()+"() ---");
+		System.out.println("1 - MONTAR FICHA");
+		System.out.println("2 - CADASTRAR EXERCÍCIOS");
+		System.out.println("3 - ALTERAR USUARIO");
 		
+		int opcao = Console.readInt("Selecione a opção desejada:");
+		
+		switch(opcao) {
+			case 1:				
+				break;
+				
+			case 2:
+				break;				
+		}
 	}
 }
